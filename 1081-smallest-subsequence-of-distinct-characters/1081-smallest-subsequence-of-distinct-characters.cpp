51d1e83096b9c43956854d34ac33d216a02a53cb
Prince_codes <princerajput4537@gmail.com>
2026-07-19 13:15:03 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.26%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        vector<int> num(26,0);
+        vector<int> vis(26, 0);
+
+        for(int i=0; i<s.length(); i++){
+            num[s[i]-'a']++;
+        }
+        string ans = "";
+
+        for(char ch : s){
+            if(!vis[ch-'a']){
+                while(!ans.empty() && ans.back() > ch){
+                    if(num[ans.back() - 'a'] > 0){
+                        vis[ans.back() - 'a'] = 0;
+                        ans.pop_back();
+                    }
+                    else {
+                        break;
+                    }
+                }
+                vis[ch - 'a'] = 1;
+                ans.push_back(ch);
+            }
+            num[ch-'a'] -= 1;
+        }
+        return ans;
+    }
+};
