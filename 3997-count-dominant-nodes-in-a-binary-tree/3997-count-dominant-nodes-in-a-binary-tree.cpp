307c1bd79d497f00fb4e28740cf5ac59aba9a7ac
Prince_codes <princerajput4537@gmail.com>
2026-07-19 14:44:36 +0530

    Time: 50 ms (14.37%), Space: 338.2 MB (22.99%) - LeetHub

---
+++ 3997-count-dominant-nodes-in-a-binary-tree/3997-count-dominant-nodes-in-a-binary-tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int cnt = 0;
+    int dfs(TreeNode* r){
+        if(!r) return 0;
+        int l = dfs(r-> left);
+        int rr = dfs(r -> right);
+
+        if(r->val >= max(l,rr)) cnt++;
+        return max({r->val,l,rr});
+    }
+    int countDominantNodes(TreeNode* root) {
+        dfs(root);
+        return cnt;
+    }
+};
