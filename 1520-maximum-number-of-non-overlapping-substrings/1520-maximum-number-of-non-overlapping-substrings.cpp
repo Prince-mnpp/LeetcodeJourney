974c30189f2a305e81d876ea9c4b1f2e4128442f
Prince_codes <princerajput4537@gmail.com>
2026-09-18 19:46:27 +0530

    Time: 9 ms (72.97%), Space: 23.3 MB (88.07%) - LeetHub

---
+++ 1520-maximum-number-of-non-overlapping-substrings/1520-maximum-number-of-non-overlapping-substrings.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    vector<string> maxNumOfSubstrings(string s) {
+        int n = s.length();
+
+        vector<int> start(26, -1);
+        vector<int> end(26, 0);
+        vector<bool> isValid(26, false);
+
+        vector<string> result;
+
+        for(int i = 0; i < n; i++) {
+            int idx = s[i] - 'a';
+
+            if(start[idx] == -1)
+                start[idx] = i;
+
+            end[idx] = i;
+        }
+
+        for(int c = 0; c < 26; c++) {
+            if(start[c] == -1)
+                continue;
+
+            isValid[c] = true;
+
+            for(int i = start[c]; i <= end[c]; i++) {
+                int idx = s[i] - 'a';
+
+                if(start[idx] < start[c]) {
+                    isValid[c] = false;
+                    break;
+                }
+
+                end[c] = max(end[c], end[idx]);
+            }
+        }
+
+        int lastTakenStart = n;
+
+        for(int i = n - 1; i >= 0; i--) {
+            int c = s[i] - 'a';
+
+            if(!isValid[c])
+                continue;
+
+            if(i == start[c] && end[c] < lastTakenStart) {
+                result.push_back(s.substr(i, end[c] - i + 1));
+                lastTakenStart = i;
+            }
+        }
+
+        reverse(result.begin(), result.end());
+
+        return result;
+    }
+};
